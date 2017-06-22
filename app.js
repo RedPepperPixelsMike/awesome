36cc6120d401f34f23831c97bb3901ae7514f015
Mike Wight <michel.wight@hotmail.com>
2017-06-22 16:58:46 -0300

    made it better

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
     next();
+
 });
 
 app.use('/message', messageRoutes);
